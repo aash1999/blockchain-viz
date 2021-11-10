40e513fb8246c9e80d284a95a4a9075535462343
Aakash Singh <aakashsingh@Aakashs-Air.domain.name>
2021-11-10 19:01:49 +0530

    mining on backend done

--- server/blockchain.js
+++ server/blockchain.js
@@ -1,7 +1,61 @@
+const { timeStamp } = require('console');
+const { createHash } = require('crypto');
+
+
 class Blockchain{
     constructor(){
-        this.chain=[];
-        
+        this.chain=new Array();
+        var date =new Date();
+        date = date.toString();
+        this.create_block("0" , 1 , 1 ,  "Genesis Block , Created By Aakash Singh", date);
+        this.block_difficulty =4;
     }
 
+    create_block(previous_hash  , nonce , block_index  , data ,timestamp ){
+        var block = {
+            index : block_index,
+            timestamp : timestamp,
+            nonce : nonce,
+            previous_hash : previous_hash,
+            data : data
+        }
+        this.chain.push(block);
+
+        return block;
+
+    }
+
+    get_previous_block(){
+        return this.chain.slice(-1);
+    }
+
+    hash(block){
+        var blockstr =""+ block.index + block.previous_hash +  block.data +block.timeStamp+ ""+block.nonce;
+        var hash_operation = createHash('sha256').update(blockstr).digest('hex');
+        return hash_operation;
+    }
+
+    proof_of_work(block){
+        var nonce = 1
+        var check_proof = false
+
+        while(check_proof == false){
+            block.nonce= nonce;
+            var hash_operation = this.hash(block);
+
+            if(hash_operation.slice(0,this.block_difficulty) == '00000'){
+                check_proof = true;
+            }else{
+                nonce+=1;
+            }
+
+        }
+
+        return nonce;
+
+    }
+
+    
+
 }
+module.exports.Blockchain = Blockchain ;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,40 @@
+//const bodyParser = require("body-parser");
 const express = require("express");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-
-
+app.use(express.urlencoded({ extended: true }));
+//app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+
+var blockchainFile = require("./blockchain.js")
+var blockchain = new blockchainFile.Blockchain();
+
+
+
+app.get("/mineblock", (req,res)=>{
+
+    var previous_block =  blockchain.chain.slice(-1)[0];
+    var previous_hash =  blockchain.hash(previous_block)
+    var data  = req.body.data;
+    console.log(req.body.data)
+    var date =new Date();
+    date = date.toString();
+    var block  = {
+      index : blockchain.chain.length +1,
+      timestamp : date,
+      nonce : 1,
+      previous_hash : previous_hash,
+      data : data
+    }
+    block.nonce  = blockchain.proof_of_work(block);
+    blockchain.create_block(block.previous_hash  , block.nonce , block.index  , block.data ,block.timestamp );
+    res.send(blockchain.chain);
+
+
+})
